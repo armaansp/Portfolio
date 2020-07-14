910354d5231c182b350d87dcee98922c8918e5c1
Md Miraj Ansari <mirajhad@users.noreply.github.com>
2020-07-14 18:35:14 +0530

    Add files via upload

---
+++ precache-manifest.2bfca4e509c641d2d411ee94fd823173.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d84423501ad46b44975f0acdb2a3ba0e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5a24543dc430bc10a895",
+    "url": "/static/css/main.445cab94.chunk.css"
+  },
+  {
+    "revision": "e44729af9527b5886190",
+    "url": "/static/js/2.2494d31c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.2494d31c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a24543dc430bc10a895",
+    "url": "/static/js/main.7b1c57f4.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  }
+]);
